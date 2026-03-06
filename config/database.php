0b8be1c95fc7115fa0f4907dda7c2293ca09760b
Alejandro Perez <apermil771@g.educaand.es>
2026-03-06 14:56:45 +0100

    añadido la conexion a la base de datos en localhost desde php

--- config/database.php
+++ config/database.php
@@ -1,17 +1,22 @@
 <?php
+// Configuración de la conexión a la base de datos
 
-$host = "localhost";
-$dbname = "happymami";
-$username = "root";
-$password = "";
+// variables de conexión
+$host = "localhost"; // Host de la base de datos
+$dbname = "happymami"; // Nombre de la base de datos
+$username = "root"; // Nombre de usuario
+$password = "root"; // Contraseña
 
+// Conexión a la base de datos con PDO
 try {
+    // Creo un objeto PDO con la conexión a la base de datos
     $conexion = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
 
-    // Mostrar errores
+    // Establezco el modo de error en excepciones
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch (PDOException $e) {
+    // Si hay un error en la conexión, muestro el mensaje de error
     echo "Error de conexión: " . $e->getMessage();
     exit;
 }
